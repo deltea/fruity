cd41cf05904c34b48bf7198b7ff0438e6fdd1cd8
deltea <thcheetah777@gmail.com>
2026-01-03 00:10:11 +0800

    welcome to fruity (looks ugly)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "./layout.css";
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>Fruity!</title>
+</svelte:head>
+
 {@render children()}
