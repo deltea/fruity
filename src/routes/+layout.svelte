fc0dd4fc5b57d6fb986d2ad007242fb58f7a738c
deltea <thcheetah777@gmail.com>
2026-01-04 00:04:04 +0800

    decoration!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,6 @@
 	<title>Fruity!</title>
 </svelte:head>
 
-<main class="bg-[url('/background.svg')] bg-repeat h-screen">
+<main class="bg-[url('/background.svg')] bg-repeat min-h-screen">
 	{@render children()}
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,72 @@
-<div class="p-8">
-  Welcome to
-  <span class="text-accent">Fruity!</span>
+<div class="flex justify-center h-full p-6">
+  <div class="w-2xl">
+    <div class="flex justify-between mb-16">
+      <div class="flex items-end gap-2">
+        <span class="text-accent inline-flex gap-1.5 items-baseline text-3xl font-black">
+          <img src="/logo-outline.png" alt="Fruity Logo" class="h-8">
+          Fruity!
+        </span>
+        by Hack Club
+      </div>
+
+      <a href="https://fruity.fillout.com/rsvp" class="h-10 flex items-center font-extrabold bg-accent px-4 rounded-lg duration-100 hover:-translate-y-1">
+        Sign Up!
+      </a>
+    </div>
+
+    <div>
+      Do <i>you</i> like music? (yes you do)
+      Are <i>you</i> interested in making music?
+      Have <i>you</i> heard of FL Studio?
+      If you answered yes to all the above (or not), then this YSWS is for you!
+
+      <br><br>
+
+      <div class="text-center">
+        presenting...
+        <br>
+        <span class="font-extrabold text-2xl">Fruity!</span>
+      </div>
+
+      <br><br>
+
+      A new hack club YSWS (you ship we ship) focused on digital projects related to music! Make a music related coding project, get FL Studio!
+
+      <br><br>
+
+      <h3 class="font-extrabold text-2xl mb-4">You Ship</h3>
+
+      Any digital project related to music! Could be a website, app, game, etc! Some ideas include:
+
+      <ul class="list-disc list-inside">
+        <li>A rhythm game</li>
+        <li>Last.fm integration</li>
+        <li>Beat maker</li>
+        <li>Or whatever else you think of!</li>
+      </ul>
+
+      <br>
+
+      <h3 class="font-extrabold text-2xl mb-4">We Ship</h3>
+      Get
+      <a class="underline decoration-2 text-accent" href="https://www.image-line.com/">FL Studio</a>
+      (Fruity Edition) to start your music creation journey!
+
+      <br><br>
+
+      <div class="flex justify-center">
+        <a
+          href="https://fruity.fillout.com/rsvp"
+          target="_blank"
+          class="h-10 inline-flex items-center font-extrabold bg-accent px-4 rounded-lg duration-100 hover:-translate-y-1"
+        >
+          RSVP now!
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <a href="https://hackclub.com/" target="_blank">
+    <img src="/flag-orpheus-top.svg" alt="Hack Club Logo" class="absolute left-12 top-0 h-16 hover:rotate-12 duration-100" />
+  </a>
 </div>
