05526f8746d1b4ae4bf7388ce576d4c171758ee1
deltea <thcheetah777@gmail.com>
2026-01-04 21:26:48 +0800

    moving background!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 	import "./layout.css";
 	import "iconify-icon";
 
+  const bgSpeed = 0.5;
+  let bgOffset = $state(0);
+
 	let { children } = $props();
+
+	onMount(() => {
+    setInterval(() => {
+      bgOffset += bgSpeed;
+    }, 20);
+  });
 </script>
 
 <svelte:head>
 	<title>Fruity!</title>
 </svelte:head>
 
-<div class="bg-[url('/background.svg')] bg-repeat min-h-screen">
+<div
+	style="background-position: {bgOffset}px {bgOffset}px;"
+	class="bg-[url('/background.svg')] bg-repeat min-h-screen"
+>
 	{@render children()}
 </div>
